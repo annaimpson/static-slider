d92573380d75c65012f0bb40070cf6417467eb0c
Anna Impson <annaimpson@gmail.com>
2019-10-24 16:00:09 -0400

    implemented two part slider using slider js

---
+++ main.js
@@ -0,0 +1,16 @@
+var quoteSwiper = new Swiper('.quote-slider', {
+    effect: 'fade',
+    allowTouchMove: false,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+var imageSwiper = new Swiper('.image-slider', {
+    allowTouchMove: false,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
